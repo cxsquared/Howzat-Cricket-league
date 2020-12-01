5bd1b0b48e7b8797335d8db3421019d736a851f9
Cody Claborn <cxsquared@gmail.com>
2020-11-30 22:26:53 -0500

    Started adding team card

--- js/src/forum/components/teams/TeamCard.js
+++ js/src/forum/components/teams/TeamCard.js
@@ -1,1 +1,15 @@
 import app from 'flarum/app';
+import Component from "flarum/Component";
+
+export default class TeamCard extends Component {
+    oninit(vnode) {
+        super.oninit(vnode);
+        this.team = this.attrs.team;
+    }
+
+    view() {
+        return <div className="TeamCard">
+            {this.team.name()}
+        </div>
+    }
+}
